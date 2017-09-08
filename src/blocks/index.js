c619755df7e2d84b40e6b75847ec85115d8ee379
Nemanja Nedeljković <nemanjan00@gmail.com>
2017-09-08 23:48:03 +0200

    Battery

--- src/blocks/index.js
+++ src/blocks/index.js
@@ -1,9 +1,38 @@
 module.exports = function(block){
 	var blocks = {
 		time: function(block){
-			return {
-				text: new Date() + ""
-			}
+			return new Promise((resolve, reject) => {
+				resolve({
+					text: new Date() + ""
+				});
+			});
+		},
+		battery: function(block){
+			return new Promise((resolve, reject) => {
+				const fs = require("fs");
+
+				fs.readFile("/sys/class/power_supply/BAT0/uevent", (err, data) => {
+					data = (data+"").split("\n");
+
+					let values = {};
+
+					data.forEach(function(line){
+						let value = line.split("=");
+
+						values[value[0]] = value[1];
+					})
+
+					var percentage = Math.round(values["POWER_SUPPLY_ENERGY_NOW"] / values["POWER_SUPPLY_ENERGY_FULL"] * 100);
+
+					if(percentage > 100){
+						percentage = 100;
+					}
+
+					resolve({
+						text: "Battery left: "+percentage
+					});
+				});
+			});
 		}
 	};
 
--- src/i3status/index.js
+++ src/i3status/index.js
@@ -8,30 +8,33 @@ const status = {
 	render: function(){
 		let output = status.blocks.map(status.renderBlock);
 
-		if(!status.inited){
-			status.inited = true;
+		Promise.all(output).then((output) => {
+			if(!status.inited){
+				status.inited = true;
 
-			console.output("{\"version\":1}");
-			console.output("[");
-			console.output(JSON.stringify(output));
-		} else {
-			console.output(",", JSON.stringify(output));
-		}
-
+				console.output("{\"version\":1}");
+				console.output("[");
+				console.output(JSON.stringify(output));
+			} else {
+				console.output(",", JSON.stringify(output));
+			}
+		});
 	},
 	renderBlock(block){
-		var result = blocks(block);
-
-		return {
-			"name":"disk_info",
-			"instance":"/home",
-			"markup":"none",
-			"full_text": " "+result.text+" ",
-			"color": block.color,
-			"background": block.backgroundColor,
-			"separator": false,
-			"separator_block_width": 0
-		};
+		return new Promise((resolve, reject) => {
+			var result = blocks(block).then((result) => {
+				resolve({
+					"name":"disk_info",
+					"instance":"/home",
+					"markup":"none",
+					"full_text": " "+result.text+" ",
+					"color": block.color,
+					"background": block.backgroundColor,
+					"separator": false,
+					"separator_block_width": 0
+				});
+			});
+		});
 	},
 	addBlock: function(block){
 		status.blocks.push(block);
